4f9cf340c01356ff397a5278df54d69949eed1ce
Fernando Terra <fernandoterra.dev@gmail.com>
2026-04-30 10:48:18 -0300

    feat: initial clean architecture structure

---
+++ src/Infrastructure/DependencyInjection.cs
@@ -0,0 +1,29 @@
+using Application.Common.Interfaces;
+using Domain.Repositories;
+using Infrastructure.Persistence;
+using Infrastructure.Persistence.Repositories;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Infrastructure;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddInfrastructure(
+        this IServiceCollection services,
+        IConfiguration configuration)
+    {
+        services.AddDbContext<ApplicationDbContext>(options =>
+            options.UseSqlServer(
+                configuration.GetConnectionString("DefaultConnection"),
+                b => b.MigrationsAssembly(typeof(ApplicationDbContext).Assembly.FullName)));
+
+        services.AddScoped<IApplicationDbContext>(provider =>
+            provider.GetRequiredService<ApplicationDbContext>());
+
+        services.AddScoped<IProductRepository, ProductRepository>();
+
+        return services;
+    }
+}
