58229267cf649c40dc76f9baf481c6ede15e3336
Fernando Terra <fernandoterra.dev@gmail.com>
2026-04-30 10:47:51 -0300

    feat: initial clean architecture structure

---
+++ src/Application/Products/Commands/CreateProduct/CreateProductCommandValidator.cs
@@ -0,0 +1,23 @@
+using FluentValidation;
+
+namespace Application.Products.Commands.CreateProduct;
+
+public sealed class CreateProductCommandValidator : AbstractValidator<CreateProductCommand>
+{
+    public CreateProductCommandValidator()
+    {
+        RuleFor(x => x.Name)
+            .NotEmpty().WithMessage("Name is required.")
+            .MaximumLength(200).WithMessage("Name must not exceed 200 characters.");
+
+        RuleFor(x => x.Description)
+            .NotEmpty().WithMessage("Description is required.")
+            .MaximumLength(1000).WithMessage("Description must not exceed 1000 characters.");
+
+        RuleFor(x => x.Price)
+            .GreaterThan(0).WithMessage("Price must be greater than zero.");
+
+        RuleFor(x => x.StockQuantity)
+            .GreaterThanOrEqualTo(0).WithMessage("Stock quantity must be non-negative.");
+    }
+}
