67b5939a9d8d01510bea04978c632322ae342c38
Fernando Terra <fernandoterra.dev@gmail.com>
2026-04-30 10:48:12 -0300

    feat: initial clean architecture structure

---
+++ src/Domain/Repositories/IProductRepository.cs
@@ -0,0 +1,12 @@
+using Domain.Entities;
+
+namespace Domain.Repositories;
+
+public interface IProductRepository
+{
+    Task<Product?> GetByIdAsync(Guid id, CancellationToken cancellationToken = default);
+    Task<IEnumerable<Product>> GetAllAsync(CancellationToken cancellationToken = default);
+    Task AddAsync(Product product, CancellationToken cancellationToken = default);
+    void Update(Product product);
+    void Delete(Product product);
+}
